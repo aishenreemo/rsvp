a5d89c455940176746830a9dab3fcf9a9366273a
Aishen Reemo <aishenreemo@tilde.club>
2026-03-24 19:36:37 +0800

    fix typo error

--- src/contexts/WeddingContext.tsx
+++ src/contexts/WeddingContext.tsx
@@ -25,7 +25,7 @@ const weddingConfig: WeddingConfig = {
     weddingDate: new Date("2026-05-26T16:00:00"),
     ceremonyTime: "2:00 PM",
     venue: {
-        name: "Savannah Farm Tagaytay",
+        name: "Savanna Farm Tagaytay",
         address: "007 Barangay, Alfonso, Cavite",
         mapUrl: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3869.585407034925!2d120.88673120000001!3d14.101634400000002!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x33bd9d1d436c328f%3A0x7ae344125ba4589a!2sSavanna%20Farm%20Tagaytay!5e0!3m2!1sen!2sph!4v1770865953692!5m2!1sen!2sph",
     },
